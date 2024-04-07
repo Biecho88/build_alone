1ae3331440e6eee64547f2cc7be7e4ab611d6bdc
Piotr Bieszczad <biecho88@gmail.com>
2024-04-07 18:43:59 +0000

    Created excavator site and added excavator for rentin to the data

---
+++ excavator/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Excavator(models.Model):
+
+    name = models.CharField(max_length=254)
+    model = models.CharField(max_length=254)
+    production_year = models.DecimalField(max_digits=6, decimal_places=2)
+    weight = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
