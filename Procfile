feaf494dac7dfef6d55b936ef634b60d98db09a7
Piotr Bieszczad <biecho88@gmail.com>
2024-04-25 20:41:19 +0000

    Connected with ElephantSQL and Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn build_alone.wsgi.application
--- build_alone/settings.py
+++ build_alone/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-^4gw@s_u+)(zp)uvkp7r3tx=r)u_@v5xa0v(bkxd%2kfk@*!-h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-biecho88-buildalone-tn4emf53brr.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-biecho88-buildalone-tn4emf53brr.ws-eu110.gitpod.io', 'build-alone-782f5267ecce.herokuapp.com']
 
 
 # Application definition
@@ -110,7 +111,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -124,12 +125,19 @@ WSGI_APPLICATION = 'build_alone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
