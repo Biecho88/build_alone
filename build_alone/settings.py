2f57c8c25a5c78ff6665cd7a5be23aaab69f4938
Piotr Bieszczad <biecho88@gmail.com>
2024-04-26 05:35:21 +0000

    Connection with Heroku works. Changed secret_key

--- build_alone/settings.py
+++ build_alone/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^4gw@s_u+)(zp)uvkp7r3tx=r)u_@v5xa0v(bkxd%2kfk@*!-h'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-biecho88-buildalone-tn4emf53brr.ws-eu110.gitpod.io', 'build-alone-782f5267ecce.herokuapp.com']
 
